a9cc08b63696bca37d3bff1582a9d7574a94740d
Jaime B. <65511374+jaosber@users.noreply.github.com>
2026-03-29 23:37:00 -0500

    Update surface-eval.js

--- tests/surface-eval.js
+++ tests/surface-eval.js
@@ -14,7 +14,7 @@ function factorial(n) {
   if (n < 0) return NaN;
   if (n === 0 || n === 1) return 1;
   n = Math.round(n);
-  var result = 1;
+  let result = 1;
   for (var i = 2; i <= n; i++) result *= i;
   return result;
 }
