37bcb7e595c662981c1d2e22c190f2cf69fe6586
Jaime B. <65511374+jaosber@users.noreply.github.com>
2026-04-22 21:37:03 -0500

    Update surface-eval.js

--- tests/surface-eval.js
+++ tests/surface-eval.js
@@ -10,6 +10,7 @@ function saturate(x) { return clamp(x, 0.0, 1.0); }
 function smoothstep(x,a,b) { let y = saturate((x-a)/(b-a)); return 3.0*y*y - 2.0*y*y*y; }
 function fract(x) { return x - Math.floor(x); }
 function sigmoid(x){ return 1/(1+Math.exp(-x)); }
+function eml(x,y) { return Math.exp(x)-Math.log(y); }
 function factorial(n) {
   if (n < 0) return NaN;
   if (n === 0 || n === 1) return 1;
