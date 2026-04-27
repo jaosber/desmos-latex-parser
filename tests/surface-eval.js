68a937e195a381ab80b7d14823e6c7c780195bd1
Jaime B. <65511374+jaosber@users.noreply.github.com>
2026-04-26 23:20:54 -0500

    Update surface-eval.js

--- tests/surface-eval.js
+++ tests/surface-eval.js
@@ -5,7 +5,7 @@
 function mod(x, y) { return x - y*Math.floor(x/y); }
 function step(x, a) { return (x < a) ? 0.0 : 1.0; }
 function lerp(x,a,b) { return a + (b-a)*x; }
-function clamp(x,a,b) { if(x < a) return a; if(x > b) return b; return x; }
+function clamp(x, a, b) { if(x < a) return a; if(x > b) return b; return x; }
 function saturate(x) { return clamp(x, 0.0, 1.0); }
 function smoothstep(x, a, b) { let y = saturate((x-a)/(b-a)); return 3.0*y*y - 2.0*y*y*y; }
 function smootherstep(x, a, b) { let y = saturate((x - a) / (b - a)); return y * y * y * (y * (y * 6.0 - 15.0) + 10.0); }
