f445cce259239bf1df4d8af0c14d536488512e0c
Jaime B. <65511374+jaosber@users.noreply.github.com>
2026-05-25 22:56:20 -0500

    Update surface-eval.js

--- tests/surface-eval.js
+++ tests/surface-eval.js
@@ -20,7 +20,7 @@ function atan2(y, x) { return Math.atan2(y,x); }
 function wrap(x, a, b) { const range = b - a; return x - range * Math.floor((x - a)/range); }
 function map(x, a, b, c, d) {
     let t = (x - a)/(b - a);
-    let y = c + (d-c)*t;
+    let y = c + (d - c)*t;
     return y;
     //if (!clamped) return y;
     //return (c < d) ? clamp(y, c, d) : clamp(y, d, c);
